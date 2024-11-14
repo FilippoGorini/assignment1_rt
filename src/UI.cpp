d0564aec95cd3dff2f4b1cf07d7bce2399ee8170
Filippo <filippogorini7@gmail.com>
2024-11-14 21:39:47 +0000

    Add distance calculation and relative topic

--- src/UI.cpp
+++ src/UI.cpp
@@ -8,7 +8,19 @@
 #include "turtlesim/SetPen.h"        
 #include "geometry_msgs/Twist.h"                // Include Twist message structure
 
-bool inputInvalid();                            // Declare isinputInvalid function
+
+bool inputInvalid()
+{
+    if (std::cin.fail())
+    {
+        std::cout << "\nINVALID INPUT!: input velocity must be a number\n";
+        std::cin.clear();  
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');     // Needed to clear all invalid characters in the buffer until \n
+        return true;
+    }
+    return false;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -16,10 +28,10 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     // disable pen of turtle1 just to not leave a trail when teleporting turtle1
-    // ros::ServiceClient pen1_client = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
-    // turtlesim::SetPen pen1_srv;
-    // pen1_srv.request.off = 1;  
-    // pen1_client.call(pen1_srv);
+    ros::ServiceClient pen1_client = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
+    turtlesim::SetPen pen1_srv;
+    pen1_srv.request.off = 1;  
+    pen1_client.call(pen1_srv);
 
     // Moving the turtle1 to another starting position instead of the center (Not needed, just for looks)
     ros::ServiceClient teleport1_client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
@@ -30,9 +42,13 @@ int main(int argc, char **argv)
     teleport1_client.waitForExistence();     
     teleport1_client.call(teleport1_srv);
 
-    // reenable trail
-    // pen1_srv.request.off = 0;          
-    // pen1_client.call(pen1_srv);
+    // reenable trail with original trail color and size
+    pen1_srv.request.r = 0xb3; 
+    pen1_srv.request.g = 0xb8;  
+    pen1_srv.request.b = 0xff;
+    pen1_srv.request.width = 3;
+    pen1_srv.request.off = 0;          
+    pen1_client.call(pen1_srv);
 
     // Spawning the new turtle:
     ros::ServiceClient spawn_client = nh.serviceClient<turtlesim::Spawn>("/spawn"); // Creates a spawn client
@@ -55,6 +71,7 @@ int main(int argc, char **argv)
     // Start the loop that checks for user commands and publish on cmd_vel topics
     while (ros::ok())
     {
+        ros::spinOnce();  
         std::string turtle_choice;
         std::cout << "\nEnter 1 or 2 to select which turtle to control (turtle1 or turtle2): ";
         std::cin >> turtle_choice;
@@ -88,22 +105,7 @@ int main(int argc, char **argv)
         if (turtle_choice!="1" && turtle_choice!="2")
         {
             std::cout << "\nINVALID INPUT!: you must enter either 1 or 2\n";
-        }
-
-        ros::spinOnce();   
+        } 
     }
     return 0;
-}
-
-
-bool inputInvalid()
-{
-    if (std::cin.fail())
-    {
-        std::cout << "\nINVALID INPUT!: input velocity must be a number\n";
-        std::cin.clear();  
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');     // Needed to clear all invalid characters in the buffer until \n
-        return true;
-    }
-    return false;
 }
--- src/distance.cpp
+++ src/distance.cpp
@@ -1,18 +1,54 @@
 #include "ros/ros.h"
 #include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "std_msgs/Float32.h"
+#include<cmath>
 
-void check_turtles(void)
+turtlesim::Pose pose1;
+turtlesim::Pose pose2;
+bool is_pose1_updated = false;
+bool is_pose2_updated = false;
+ros::Publisher pub_distance;
+
+
+void pubNewDistance()
 {
-    int a = 2;          // placeholder
+    float dx = pose1.x - pose2.x;
+    float dy = pose1.y - pose2.y;
+    float distance = std::sqrt(dx * dx + dy * dy);
+    
+    ROS_INFO("Distance: %f", distance);
+    std_msgs::Float32 distance_msg;
+    distance_msg.data = distance;
+    pub_distance.publish(distance_msg);
+    is_pose1_updated = false;
+    is_pose2_updated = false;
 }
 
+void checkTurtle1(const turtlesim::Pose::ConstPtr& msg)
+{
+    pose1 = *msg;
+    is_pose1_updated = true;
+    if (is_pose2_updated)
+        pubNewDistance();
+}
 
+void checkTurtle2(const turtlesim::Pose::ConstPtr& msg)
+{
+    pose2 = *msg;
+    is_pose2_updated = true;
+    if (is_pose1_updated)
+        pubNewDistance();
+}
+
+geometry_msgs::Twist stop_msg;
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "bounds_checker");
-    // ros::NodeHandle nh;
-    // ros::Subscriber sub1 = nh.subscribe("/turtle1/pose/", 10, check_turtles);
-    // ros::Subscriber sub2 = nh.subscribe("/turtle2/pose/", 10, check_turtles);
+    ros::init(argc, argv, "distance");
+    ros::NodeHandle nh;
+    ros::Subscriber sub1 = nh.subscribe("/turtle1/pose", 10, checkTurtle1);
+    ros::Subscriber sub2 = nh.subscribe("/turtle2/pose", 10, checkTurtle2);
+    pub_distance = nh.advertise<std_msgs::Float32>("turtle_distance", 10);
     ros::spin();
     return 0;
 }
